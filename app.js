3f146a08ccbe6ffce010e68c3c158a4c305a46b8
Dos <DosDeosos@gmail.com>
2023-09-03 22:59:45 +0700

    API Versioning Setup

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
-import productRouter from "./routes/products.js";
 import swaggerfile from "./swagger-docs/swagger.json" assert { type: "json" };
+
+import v1AdminProductRouter from "./routes/v1/admin/products.js";
+
+import v1UserProductRouter from "./routes/v1/user/products.js";
 
 const app = express();
 const port = 4000;
@@ -11,7 +14,10 @@ const port = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerfile));
-app.use("/products", productRouter);
+
+app.use("/v1/admin/products", v1AdminProductRouter);
+
+app.use("/v1/user/products", v1UserProductRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ routes/v1/admin/products.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+const router = new Router();
+
+router.get("/", (req, res) => {
+  // #swagger.tags = ["Admin/Products"]
+  // #swagger.summary = "Get all products"
+  res.send("Hello, world!");
+});
+
+router.get("/:id", (req, res) => {
+  // #swagger.tags = ["Admin/Products"]
+  // #swagger.summary = "Get products by id"
+  res.send("Hello, world!");
+});
+
+router.post("/", (req, res) => {
+  // #swagger.tags = ["Admin/Products"]
+  // #swagger.summary = "Create products"
+  const { name, price } = req.body;
+  res.send("Hello, world!");
+});
+export default router;
