642b80b3c9f7f60584242191b088bb8b4e84ec95
BlackCat <neko.sword@gmail.com>
2023-09-11 20:02:53 +0700

    add: API search, sort and pagination in services

--- routes/v1/admin/services.js
+++ routes/v1/admin/services.js
@@ -2,55 +2,57 @@ import { Router } from "express";
 import { prisma } from "../../../lib/db.js";
 const router = new Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
 	// #swagger.tags = ["Admin/Service"]
-  // #swagger.summary = "Get all services"
-	const search = req.query.search || ''
-	const category = req.query.category || ''
-	const sort = req.query.sort || ''
+	// #swagger.summary = "Get all services"
+	const search = req.query.search;
+	const category = req.query.category;
+	const sort = req.query.sort;
+	const price = +req.query.price;
+	const page = +req.query.page || 1;
+
+	const PAGE_SIZE = 3;
+	const skip = PAGE_SIZE * (page - 1);
 
 	try {
-		// const allServices = await prisma.services.findMany({
-		// 	include: {
-		// 		category: {
-		// 			select: {
-		// 				category_name: true
-		// 			}
-		// 		}
-		// 	},
-		// 	where: {
-		// 		OR: [
-		// 			{category_name: search},
-		// 			{category_name: category}
-		// 		]
-		// 	},
-		// 	orderBy: {
-		// 		category_name: { sort: sort, null: 'asc' }
-		// 	}
-		// })
-
-		// res.json({
-		// 	data: allServices
-		// })
+		const serviceCount = await prisma.services.count();
 		const allServices = await prisma.services.findMany({
+			take: PAGE_SIZE,
+			skip: skip,
+			where: {
+				service_name: {
+					contains: search, // ใช้ contains เพื่อให้คำค้นมีค่าเหมือน RegExp
+				},
+				category: {
+					category_name: {
+						contains: category
+					}
+				},
+			},
 			include: {
 				category: {
 					select: {
-						category_name: true
-					}
-				}
+						category_name: true,
+					},
+				},
+			},
+			orderBy: {
+				service_name: sort
 			}
-		})
+		});
+		const listServices = allServices.map((service) => ({
+			...service,
+			category: service.category.category_name,
+		}));
 		res.json({
-			data: allServices
-		})
+			data: listServices,
+			totalServices: serviceCount,
+		});
 	} catch (error) {
-		res.status(400).send(error)
+		res.status(400).send(error);
 	}
-})
+});
 
-router.post('/create', async (req, res) => {
-	
-})
+router.post("/create", async (req, res) => {});
 
 export default router;
