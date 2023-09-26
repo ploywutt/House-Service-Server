7daca7a3de2d82c0cff186b2e615089863bfab66
Guer <maythee.srianut@gmail.com>
2023-09-26 23:48:38 +0700

    fix: add service name in subsubvice api

--- routes/v1/user/subservices.js
+++ routes/v1/user/subservices.js
@@ -4,32 +4,37 @@ import { prisma } from "../../../lib/db.js";
 const router = new Router();
 
 router.get("/:id", async (req, res) => {
-	// #swagger.tags = ["User/Service"]
+  // #swagger.tags = ["User/Service"]
   // #swagger.summary = "Get sub-service by id"
-	const reqId = Number(req.params.id);
-	try {
-		const data = await prisma.sub_services.findMany({
-			where: {
-				services: {
-					id: reqId,
-				},
-			},
-			select: {
-				sub_service_name: true,
-				price_per_unit: true,
-				unit: true,
-			},
-		});
-		res.json({
-			data: data,
-		});
-	} catch (error) {
-		console.error(error);
-		res.json({
-			message: "Error fetching order details:",
-			error,
-		});
-	}
+  const reqId = Number(req.params.id);
+  try {
+    const data = await prisma.sub_services.findMany({
+      where: {
+        services: {
+          id: reqId,
+        },
+      },
+      select: {
+        sub_service_name: true,
+        price_per_unit: true,
+        unit: true,
+        services: {
+          select: {
+            service_name: true,
+          },
+        },
+      },
+    });
+    res.json({
+      data: data,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      message: "Error fetching order details:",
+      error,
+    });
+  }
 });
 
 export default router;
