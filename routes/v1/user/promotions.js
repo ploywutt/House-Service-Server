1564f6da1fc86ae34d55f2431a99267b7891b4c3
Guer <maythee.srianut@gmail.com>
2023-09-28 21:52:39 +0700

    feat: promotion calculate (ongoing)

--- routes/v1/user/promotions.js
+++ routes/v1/user/promotions.js
@@ -3,25 +3,25 @@ import { prisma } from "../../../lib/db.js";
 const router = new Router();
 
 router.get("/:id", async (req, res) => {
-	// #swagger.tags = ["Admin/Promotions"]
-	// #swagger.summary = "Get Promotion code by id"
-	const promotionId = req.params.id
-	try {
-		const promotion = await prisma.promotions.findUnique({
-			where: {
-				promotion_code: promotionId
-			}
-		})
+  // #swagger.tags = ["Admin/Promotions"]
+  // #swagger.summary = "Get Promotion code by id"
+  const promotionId = req.params.id;
+  console.log(promotionId);
+  try {
+    const promotion = await prisma.promotions.findUnique({
+      where: {
+        promotion_code: promotionId,
+      },
+    });
 
-		res.json({
-			data: promotion
-		})
-	} catch (error) {
-		res.json({
-			message: error
-		})
-	}
-})
+    res.json({
+      data: promotion,
+    });
+  } catch (error) {
+    res.json({
+      message: error,
+    });
+  }
+});
 
-
-export default router; 
+export default router;
